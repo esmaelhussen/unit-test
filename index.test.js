d3d06165c22b1dcfa0afbcf019f53eb16d0f5c69
esmaelhussen <esmaelhussenabdu@gmail.com>
2024-11-15 09:28:07 +0300

    modify the test file of js

--- index.test.js
+++ index.test.js
@@ -24,27 +24,27 @@ test("checking reverse string", () => {
 describe("Calculator ", () => {
   const calculator = new Calculator();
   describe("sum of two numbers", () => {
-    test("Test 1", () => expect(calculator.add(10, 5)).toEqual(15));
-    test("Test 2", () => expect(calculator.add(20, 10)).toEqual(30));
-    test("Test 3", () => expect(calculator.add(30, 15)).toEqual(45));
+    test("Test 1", () => expect(calculator.add(25, 15)).toEqual(40));
+    test("Test 2", () => expect(calculator.add(10, 15)).toEqual(25));
+    test("Test 3", () => expect(calculator.add(14, 15)).toEqual(29));
   });
 
   describe("Difference of Two Numbers", () => {
-    test("Test 1", () => expect(calculator.sub(10, 5)).toEqual(5));
-    test("Test 2", () => expect(calculator.sub(20, 10)).toEqual(10));
-    test("Test 3", () => expect(calculator.sub(30, 15)).toEqual(15));
+    test("Test 1", () => expect(calculator.sub(25, 5)).toEqual(20));
+    test("Test 2", () => expect(calculator.sub(10, 10)).toEqual(0));
+    test("Test 3", () => expect(calculator.sub(35, 15)).toEqual(20));
   });
 
   describe("product of Two Numbers", () => {
-    test("Test 1", () => expect(calculator.mult(10, 5)).toEqual(50));
-    test("Test 2", () => expect(calculator.mult(20, 10)).toEqual(200));
-    test("Test 3", () => expect(calculator.mult(30, 15)).toEqual(450));
+    test("Test 1", () => expect(calculator.mult(5, 5)).toEqual(25));
+    test("Test 2", () => expect(calculator.mult(10, 10)).toEqual(100));
+    test("Test 3", () => expect(calculator.mult(10, 15)).toEqual(150));
   });
 
   describe("Divide of two Numbers", () => {
-    test("Test 1", () => expect(calculator.div(10, 5)).toEqual(2));
-    test("Test 2", () => expect(calculator.div(20, 10)).toEqual(2));
-    test("Test 3", () => expect(calculator.div(30, 15)).toEqual(2));
+    test("Test 1", () => expect(calculator.div(20, 5)).toEqual(4));
+    test("Test 2", () => expect(calculator.div(40, 40)).toEqual(1));
+    test("Test 3", () => expect(calculator.div(45, 15)).toEqual(3));
   });
 });
 
